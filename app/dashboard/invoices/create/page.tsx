70804fd6844b0d9cc8840f82ecc77fb5869751f4
Ian-xyz9 <160653171+Ian-xyz9@users.noreply.github.com>
2024-02-29 21:27:45 +0000

    Up to and including completion of chapter 15 adding authentication. Failing to connect on localhost

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -21,5 +21,3 @@ export default async function Page() {
     </main>
   );
 }
-
-//Adding something 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-module.exports = nextConfig;
+/** This is needed to run on replit **/
+module.exports = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['3406d0ba-2452-438d-ae18-64541cf5e237-00-3o4a7nixg9qaw.janeway.repl.co'],
+    },
+  },
+}
